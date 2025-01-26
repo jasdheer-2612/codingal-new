b2bb85cc07ea47319c0d33e762e71aa26ec5f67c
jasdheer <jasdheer2612@gmail.com>
2025-01-26 22:29:05 +0530

    commit

---
+++ lesson2/main.py
@@ -0,0 +1,55 @@
+a= 45 #int
+print(a, "type", type(a))
+
+b= 7.4 
+print(b, "type", type(b))
+
+c= "python" 
+print(c, "type", type(c))
+
+d= False
+print(d, "type", type(d))
+
+
+weight = 2.1
+weight_int = int(weight)
+print(weight_int, "type", type(weight_int))
+number= 2
+number_string =str(number)
+print(number_string, "type:", type(number_string)) 
+number_string_float = float(number_string)
+print(number_string_float, "type:", type(number_string_float))
+
+num1 =4
+num2 =3
+num3 =5
+
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num3)
+print("Division:", num1 / num2)
+print("Floor division:", num1 // num2)
+print("Modulus:", num1 % num2)
+print("Exponential:", num1 ** num2)
+print("Square root:", num1 ** num3)
+
+print(num1 == num2)
+
+firstName = "jasdheer"
+lastName = "bhaiya"
+print(firstName + " " + lastName)
+
+text ="python"
+print(text * 3)
+
+word = "codingisaweb"
+print("length of a string:", len(word))
+print("First letter:", word[0])
+print("last letter:", word[5])
+print("last letter:", word[-1])
+print("First 3 letter:", word[0:4])
+print("last 3 letter:", word[3:6])
+print("Get odi:", word[1:8])
+
+
